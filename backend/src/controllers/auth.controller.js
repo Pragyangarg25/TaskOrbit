5a4ae78bbe163af13aa3ce60efa09b8cb03b75e5
Pragyan Garg <gargpragyan2005@gmail.com>
2025-08-08 16:06:56 +0530

    backend files updated

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,78 @@
+import { asyncHandler } from "../middlewares/asyncHandler.middleware.js";
+import { config } from "../config/app.config.js";
+import { registerSchema } from "../validation/auth.validation.js";
+import { HTTPSTATUS } from "../config/http.config.js";
+import { registerUserService, loginOrCreateAccountService } from "../services/auth.service.js";
+import passport from "passport";
+import { ProviderEnum } from "../enums/account-provider.enum.js";
+
+export const googleAuthCallback = asyncHandler(async (req, res) => {
+  res.redirect(config.FRONTEND_ORIGIN);
+});
+
+export const registerUserController = asyncHandler(
+  async (req, res) => {
+    const body = registerSchema.parse({
+      ...req.body,
+    });
+
+    await registerUserService(body);
+
+    return res.status(HTTPSTATUS.CREATED).json({
+      message: "User created successfully",
+    });
+  }
+);
+
+export const loginController = asyncHandler(
+  async (req, res, next) => {
+    passport.authenticate(
+      "local",
+      (
+        err,
+        user,
+        info
+      ) => {
+        if (err) {
+          return next(err);
+        }
+
+        if (!user) {
+          return res.status(HTTPSTATUS.UNAUTHORIZED).json({
+            message: info?.message || "Invalid email or password",
+          });
+        }
+
+        req.logIn(user, (err) => {
+          if (err) {
+            return next(err);
+          }
+
+          return res.status(HTTPSTATUS.OK).json({
+            message: "Logged in successfully",
+            user,
+          });
+        });
+      }
+    )(req, res, next);
+  }
+);
+
+export const logOutController = asyncHandler(
+  async (req, res) => {
+    req.logout((err) => {
+      if (err) {
+        console.error("Logout error:", err);
+        return res
+          .status(HTTPSTATUS.INTERNAL_SERVER_ERROR)
+          .json({ error: "Failed to log out" });
+      }
+    });
+
+    req.session = null;
+    res.clearCookie("session");
+    return res
+      .status(HTTPSTATUS.OK)
+      .json({ message: "Logged out successfully" });
+  }
+); 
---
+++ backend/src/models/account.model.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+import { ProviderEnum } from "../enums/account-provider.enum.js";
+
+const accountSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    provider: {
+      type: String,
+      enum: Object.values(ProviderEnum),
+      required: true,
+    },
+    providerId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    refreshToken: { type: String, default: null },
+    tokenExpiry: { type: Date, default: null },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.refreshToken;
+      },
+    },
+  }
+);
+
+const AccountModel = mongoose.model("Account", accountSchema);
+export default AccountModel; 
